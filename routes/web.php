f88e04d745c186e29677f23e9c9dd9ad3d210830
Teep-m <torino.galaxie@gmail.com>
2023-12-01 02:03:54 +0900

    breeze導入

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\Sample\IndexController;
-use App\Http\Controllers\Tweet\CreateController;
-use App\Http\Controllers\Tweet\IndexController as TweetIndexController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,16 +14,45 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//! laravelトップページ
 Route::get('/', function () {
     return view('welcome');
 });
 
+//! Laravel Breeze
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//! サンプルコード
 // Route::get('/sample', [IndexController::class, 'show']);
 
 // Route::get('/sample/{id}', [IndexController::class, 'showId']);
 
+//! 一覧表示
 Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
     ->name('tweet.index');
 
+//! 投稿処理
 Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
     ->name('tweet.create');
+
+//! 更新処理
+Route::get('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\IndexController::class)
+    ->name('tweet.update.index')
+    ->where('tweetId', '[0-9]+');
+Route::put('/tweet/update/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)
+    ->name('tweet.update.put')
+    ->where('tweetId', '[0-9]+');
+
+//! 削除処理
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)
+    ->name('tweet.delete');
+
+require __DIR__.'/auth.php';
